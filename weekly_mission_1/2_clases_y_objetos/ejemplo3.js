94124ce1e8256503bd4a5854b908602c3a2a3beb
Elian <99064398+ElianSanti@users.noreply.github.com>
2022-05-10 22:20:16 -0500

    Delete weekly_mission_1 directory

--- weekly_mission_1/2_clases_y_objetos/ejemplo3.js
+++
@@ -1,4 +0,0 @@
-// Crear un objeto de la clase Student (esto se le llama instanciación)
-const carloStudent = new Student("Carlo", 12, ["NodeJs", "Python"])
-console.log("Ejemplo 3: Instanciar un objeto con atributos")
-console.log(carloStudent)
